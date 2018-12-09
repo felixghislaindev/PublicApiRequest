6cc9945e57d6ef7978c569722e34bce43792127c
felixghislaindev <felixghislain@yahoo.com>
2018-12-09 20:54:35 +0000

    late changes

--- js/app.js
+++ js/app.js
@@ -18,9 +18,9 @@ const url ='https://randomuser.me/api/?results=12&nat=us';
         employeeArray.push( array[i])
     }
     
-    const employeesData = employeeArray.map(employee =>{
+    const employeesData = employeeArray.map((employee,index) =>{
        
-            return `  <div class="card">
+            return `  <div class="card" data-index=${index}>
             <div class="card-img-container">
                 <img class="card-img" src=${employee.picture.large} alt="profile picture">
             </div>
@@ -34,6 +34,7 @@ const url ='https://randomuser.me/api/?results=12&nat=us';
    
     }
     )
+   
     // Formaating phine number
     function formatNumber(num){
           // stripe all none number 
@@ -89,21 +90,123 @@ const url ='https://randomuser.me/api/?results=12&nat=us';
         $('#modal-prev').click((e)=>{
             const modal = e.currentTarget.parentNode.parentNode;
             // Selected modal name 
-            const selectedModalName = $('#name').html();
+            // const selectedModalName = $('#name').html();
+            let indexNum;
 
             // Cards array
             const cardsArray = document.querySelectorAll('.card');
-            cardsArray.forEach((el,i) => {
-                const cardName =  el.children[1].children[0].innerHTML;
-                if(selectedModalName === cardName){
-                    console.log(el)
+            cardsArray.forEach((el) => {
+                const cardName =  modal.children[1].children[1].innerHTML;
+                
+                if (el.children[1].children[0].innerHTML === cardName){
+                    let cardIndex = el.dataset.index
+                    indexNum = parseInt(cardIndex);
+                    const prevcard = cardsArray[indexNum - 1];
+                    const prvName =prevcard.children[1].children[0].innerHTML;
+                    
+                    employees.forEach(employee =>{
+                        const name = `${employee.name.first} ${employee.name.last}`;
+                        const employeePhone = formatNumber(employee.phone);
+            
+                        // user date of birth 
+                        const date =  dOB(employee.dob);
+                        
+                        if(name === prvName){
+                            $('.modal-container').remove();
+                            $('body').append( `<div class="modal-container">
+                            <div class="modal">
+                                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                                <div class="modal-info-container">
+                                    <img class="modal-img" src=${employee.picture.large} alt="profile picture">
+                                    <h3 id="name" class="modal-name cap">${name}</h3>
+                                    <p class="modal-text">${employee.email} </p>
+                                    <p class="modal-text cap">${employee.location.city} </p>
+                                    <hr>
+                                    <p class="modal-text">${employeePhone}</p>
+
+                                    <p class="modal-text">${employee.location.postcode} ${employee.location.state} , ${employee.location.state}, OR 97204</p>
+                                    <p class="modal-text">Birthday: ${date}</p>
+                                </div>
+                                <div class="modal-btn-container">
+                                        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                                        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                                    </div>
+                            </div>`)
+                        } 
+                    })
+                     
+                 
+                    
+
+
+
+
+                    $('#modal-close-btn').click(() => $('.modal-container').remove()); 
                 }
                 
             }) 
+            console.log(cardsArray)
         })
         // Selecting next button
-        $('#modal-next').click(()=>{
-            console.log('next')
+        $('#modal-next').click((e)=>{
+            const modal = e.currentTarget.parentNode.parentNode;
+            // Selected modal name 
+            // const selectedModalName = $('#name').html();
+            let indexNum;
+
+            // Cards array
+            const cardsArray = document.querySelectorAll('.card');
+            cardsArray.forEach((el) => {
+                const cardName =  modal.children[1].children[1].innerHTML;
+                
+                if (el.children[1].children[0].innerHTML === cardName){
+                    let cardIndex = el.dataset.index
+                    indexNum = parseInt(cardIndex);
+                    const prevcard = cardsArray[indexNum - 1];
+                    const prvName =prevcard.children[1].children[0].innerHTML;
+                    
+                    employees.forEach(employee =>{
+                        const name = `${employee.name.first} ${employee.name.last}`;
+                        const employeePhone = formatNumber(employee.phone);
+            
+                        // user date of birth 
+                        const date =  dOB(employee.dob);
+                        
+                        if(name === prvName){
+                            $('.modal-container').remove();
+                            $('body').append( `<div class="modal-container">
+                            <div class="modal">
+                                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                                <div class="modal-info-container">
+                                    <img class="modal-img" src=${employee.picture.large} alt="profile picture">
+                                    <h3 id="name" class="modal-name cap">${name}</h3>
+                                    <p class="modal-text">${employee.email} </p>
+                                    <p class="modal-text cap">${employee.location.city} </p>
+                                    <hr>
+                                    <p class="modal-text">${employeePhone}</p>
+
+                                    <p class="modal-text">${employee.location.postcode} ${employee.location.state} , ${employee.location.state}, OR 97204</p>
+                                    <p class="modal-text">Birthday: ${date}</p>
+                                </div>
+                                <div class="modal-btn-container">
+                                        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                                        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                                    </div>
+                            </div>`)
+                        } 
+                    })
+                     
+                 
+                    
+
+
+
+
+                    $('#modal-close-btn').click(() => $('.modal-container').remove()); 
+                }
+                
+            }) 
+            console.log(cardsArray)
         })
             }
         });
